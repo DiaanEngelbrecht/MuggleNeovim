5aa25ec4b3c77548a29069ae0e022da6f93418dd
Diaan Engelbrecht <diaan.engelbrecht@t-mobile.nl>
2023-03-28 17:52:46 +0200

    Fix an issue with the quickfix list

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "q", "<cmd>:cclose<cr>")
-vim.keymap.set("n", "n", "<cmd>:cnext<cr>")
-vim.keymap.set("n", "N", "<cmd>:cprev<cr>")
+vim.keymap.set("n", "q", "<cmd>:cclose<cr>", {buffer = 0})
+vim.keymap.set("n", "n", "<cmd>:cnext<cr>", {buffer = 0})
+vim.keymap.set("n", "N", "<cmd>:cprev<cr>", {buffer = 0})
