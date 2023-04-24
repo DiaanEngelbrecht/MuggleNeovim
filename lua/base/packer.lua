8a771503185ffbb6c1cd62daaf20655ce893d3c1
Diaan Engelbrecht <diaan.engelbrecht@t-mobile.nl>
2023-04-24 09:42:43 +0200

    Added even more transparency

--- lua/base/packer.lua
+++ lua/base/packer.lua
@@ -4,6 +4,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+  use 'xiyaowong/transparent.nvim'
   use 'navarasu/onedark.nvim'
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.1',
