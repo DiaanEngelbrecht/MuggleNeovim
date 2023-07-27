d5dba8b04fe5aa18ad053fdad27d7af7f2fe0b58
Diaan Engelbrecht <diaan.engelbrecht@gmail.com>
2023-07-27 22:14:46 +0200

    Fixed indent-blankline default settings on dash

--- lua/base/init.lua
+++ lua/base/init.lua
@@ -2,3 +2,16 @@ require("base.packer")
 require("base.theme")
 require("base.keybindings")
 require("base.set")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "help",
+    "dashboard",
+    "NvimTree",
+    "mason",
+    "notify",
+  },
+  callback = function()
+    vim.b.miniindentscope_disable = true
+  end,
+})
