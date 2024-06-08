509e2710dd62c7fea5218bdbc37989d94e86f140
Diaan Engelbrecht <diaan@zulzi.com>
2024-06-08 13:41:16 +0200

    working buffer order checkpoint w/o ivy

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,22 +2,15 @@ local telescope = require('telescope')
 
 local sorters = require('telescope.sorters')
 
+-- Here I sort the output of the buffer window to look more like my stack
 local custom_sorter = sorters.Sorter:new {
   scoring_function = function(entry, prompt, line)
-    vim.print("---")
-    vim.print("Line is " .. line)
     local number = tonumber(string.match(line, "%s*(%d+)%s*:"))
-    vim.print("Buffer number: " .. vim.inspect(number))
-    vim.print("List current state is below, length " .. _G.BufStack:getn())
-    _G.BufStack:list()
-    vim.print("Entry is listed " .. vim.inspect(_G.BufStack:get_index(number)))
-    local score = (_G.BufStack:getn() - _G.BufStack:get_index(number)) * 10
-    vim.print("Score " .. score)
-    vim.print("---")
-    return _G.BufStack:getn() * 1000
+    local score = (_G.BufStack:getn() - _G.BufStack:get_index(number))
+      * 10 + 10 + (line:lower():find(prompt:lower(), 1, true) or 100000)
+    return score
   end
 }
-
 
 telescope.setup {
   defaults = {
@@ -106,5 +99,5 @@ end, {})
 vim.keymap.set('n', '<leader>pf', project_files, {})
 vim.keymap.set('n', '<leader>/', function() builtin.live_grep(ivy_theme) end, {})
 vim.keymap.set('n', 'g/', function() builtin.grep_string() end, {})
-vim.keymap.set('n', '<leader>bb', function() builtin.buffers(ivy_theme) end, {})
+vim.keymap.set('n', '<leader>bb', function() builtin.buffers() end, {})
 vim.keymap.set('n', '<leader>pp', function() require 'telescope'.extensions.project.project(ivy_theme) end, {})
